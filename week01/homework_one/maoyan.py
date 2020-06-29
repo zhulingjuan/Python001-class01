0655fe563f5051e9b8fcf438917d66ced16964ba
lingjuan.zhu <1425961691@qq.com>
2020-06-29 13:35:43 +0800

    change

--- week01/homework_one/maoyan.py
+++ week01/homework_one/maoyan.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup as bs
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
-Cookie = '__mta=143547274.1593266422635.1593283558067.1593285636999.30; uuid_n_v=v1; uuid=89B2ADC0B87E11EA80F64D80EBA969F73AA3FE036ADB41CE9C408BF4E92E9FB3; _csrf=1d5301085108361c5f13cec05a41aab4c8bf42f08e093ef5b2a637ae78e385a7; mojo-uuid=9dfc62d29abe31cfca81d46f99f2d969; _lxsdk_cuid=172f6144329c8-0e20cfa8fe5329-31657402-1ea000-172f6144329c8; _lxsdk=89B2ADC0B87E11EA80F64D80EBA969F73AA3FE036ADB41CE9C408BF4E92E9FB3; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1593266422,1593266486,1593282850; _lx_utm=utm_source%3DBaidu%26utm_medium%3Dorganic; mojo-session-id={"id":"7537f3c81e43ac80dda2a10db787d287","time":1593285635987}; mojo-trace-id=2; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1593285638; __mta=143547274.1593266422635.1593285636999.1593285637688.31; _lxsdk_s=172f7396aae-be9-209-a15%7C%7C5'
+Cookie = '__mta=143547274.1593266422635.1593283558067.1593285636999.30; uuid_n_v=v1; uuid=89B2ADC0B87E11EA80F64D80EBA969F73AA3FE036ADB41CE9C408BF4E92E9FB3; mojo-uuid=9dfc62d29abe31cfca81d46f99f2d969; _lxsdk_cuid=172f6144329c8-0e20cfa8fe5329-31657402-1ea000-172f6144329c8; _lxsdk=89B2ADC0B87E11EA80F64D80EBA969F73AA3FE036ADB41CE9C408BF4E92E9FB3; _csrf=6ef2dc6760063e8d80d523c9f02a3bb1408243b5715780d5f53bf6a3611e4a54; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1593326860,1593345958,1593362244,1593403362; _lx_utm=utm_source%3DBaidu%26utm_medium%3Dorganic; mojo-session-id={"id":"ae7b70dea20b9454a86f9078ef6b8bea","time":1593408559179}; mojo-trace-id=7; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1593408839; __mta=143547274.1593266422635.1593285636999.1593408838695.31; _lxsdk_s=172fe8cfd6c-437-1de-970%7C%7C13'
 
 
 header = {
@@ -21,6 +21,7 @@ header = {
 myurl = 'https://maoyan.com/films?showType=3'
 
 response = requests.get(myurl,headers=header)
+print()
 print(response.text)
 bs_info = bs(response.text, 'html.parser')
 
@@ -35,10 +36,10 @@ for tags in bs_info.find_all('div', attrs={'class': 'movie-hover-info'}):
     movie_type = tags.find_all('div', attrs={'class': 'movie-hover-title'})[1].text
     # 上映时间
     plan_date = tags.find_all('div', attrs={'class': 'movie-hover-brief'})[0].text
-    mylist = [film_name, movie_type, plan_date]
-    movie1 = pd.DataFrame(data = mylist)
-    # windows需要使用gbk字符集
-    movie1.to_csv('./movie1.csv', encoding='utf8', index=True, header=False)
+    mylist.append([film_name, movie_type, plan_date])
+movie1 = pd.DataFrame(data = mylist)
+# windows需要使用gbk字符集
+movie1.to_csv('./movie1.csv', encoding='utf8', index=True, header=False)    
 
 
 
