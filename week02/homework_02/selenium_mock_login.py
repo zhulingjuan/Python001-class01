d02c49f8fbd5b664b44ff9a7382feb4dcc1cb5ba
lingjuan.zhu <1425961691@qq.com>
2020-07-07 01:50:17 +0800

    x

---
+++ week02/homework_02/selenium_mock_login.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+
+try:
+    browser = webdriver.Chrome()
+    # 需要安装chrome driver, 和浏览器版本保持一致
+    # http://chromedriver.storage.googleapis.com/index.html
+    
+    browser.get('https://shimo.im')
+    time.sleep(1)
+
+    btm1 = browser.find_element_by_xpath('//*[@id="homepage-header"]/nav/div[3]/a[2]/button')
+    btm1.click()
+
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div/input').send_keys('13051699392')
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys('Zhu123..')
+    time.sleep(1)
+    browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/button').click()
+
+    cookies = browser.get_cookies() # 获取cookies
+    print(cookies)
+    time.sleep(3)
+
+except Exception as e:
+    print(e)
+finally:    
+    browser.close()
+    
